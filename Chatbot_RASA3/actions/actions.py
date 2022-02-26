b82c558d3eec30676b3b424f006f26519d33d474
Bartłomiej Pałus <palusba@gmail.com>
2022-02-26 23:54:48 +0100

    development phase finished

--- Chatbot_RASA3/actions/actions.py
+++ Chatbot_RASA3/actions/actions.py
@@ -1,37 +1,28 @@
-# This files contains your custom actions which can be used to run
-# custom Python code.
-#
-# See this guide on how to implement these action:
-# https://rasa.com/docs/rasa/custom-actions
-
-
-# This is a simple example for a custom action which utters "Hello World!"
-
 # from typing import Any, Text, Dict, List
 #
 # from rasa_sdk import Action, Tracker
+# from rasa_core_sdk.events import Restarted
 # from rasa_sdk.executor import CollectingDispatcher
-# from rasa_sdk.forms import FormAction
-
-# class ActionBookingForm(FormAction):
+#
+#
+# class ActionDenial(Action):
 #
 #     def name(self) -> Text:
-#         return "action_guests_number"
+#         return "action_denial"
 #
-#     @staticmethod
-#     def required_slots(tracker: "Tracker") -> List[text]:
-#         return ["n_ppl"]
+#     def run(self, dispatcher: CollectingDispatcher,
+#             tracker: Tracker,
+#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 #
-#     def submit(
-#         self,
-#         dispatcher: "CollectingDispatcher",
-#         tracker: "Tracker",
-#         domain: "DomainDict",
-#     ) -> List[EventType]:
 #
-#         n_ppl = tracker.get_slot('n_ppl')
+#         return [Restarted()]
 #
-#         print(n_ppl)
+# class ActionRestart(Action):
 #
-#         dispatcher.utter_message(text = f"Splendid! You have a room booked for {n_ppl} people with breakfast. Payment method: credit card at reception. Can you confirm please?")
-#         return []
+#   def name(self) -> Text:
+#       return "action_restart"
+#
+#   def run(self, dispatcher, tracker, domain):
+#       # do something here
+#
+#       return []
